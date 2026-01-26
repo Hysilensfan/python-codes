0d862e2a4b281c5181e72cd9c385549f63a1d97b
Hysilensfan <uraume2027@gmail.com>
2026-01-26 12:44:46 +0800

    Update py 027.py

--- py 027.py
+++ py 027.py
@@ -1,7 +1,5 @@
-g1,g2,tie = "1","2","0"
 for i in range(int(input())):
-    s1,s2=input().split(",")
-    r = g2
+    s1,s2=input().split(",");g1,g2,tie = "1","2","0";r = g2
     if s1 == s2:
         r = tie
     elif (s1,s2) in [("O","Y"),("Y","P"),("P","O")]:
