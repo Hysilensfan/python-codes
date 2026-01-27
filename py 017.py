c720a63418f75d383f3669472633fb9c0f8c4c7b
Hysilensfan <uraume2027@gmail.com>
2026-01-27 14:46:59 +0800

    Update py 017.py

--- py 017.py
+++ py 017.py
@@ -1,3 +1,4 @@
+#first compose method
 for _ in range(int(input())):
     s = int(input());t = True
     if s < 2:
@@ -8,3 +9,16 @@ for _ in range(int(input())):
                 t = False
                 break
     print("Y"if t else"N")
+#second compose method
+def f(s):
+    if s < 2:
+        return False
+    else:
+        for i in range(2, int(s**0.5)+1):
+            if s % i == 0:
+                return False
+                break
+        return True
+for i in range(int(input())):
+    s = int(input())
+    print("Y"if f(s) else"N")
