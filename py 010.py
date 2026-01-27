70685fe9ae4608570e32f969ae111914dd1c78b5
Hysilensfan <uraume2027@gmail.com>
2026-01-27 15:53:25 +0800

    Update py 010.py

--- py 010.py
+++ py 010.py
@@ -1,12 +1,12 @@
 #first compose method
-from math import isqrt
+from math import sqrt        #"sqrt" calculates the real square root (with decimals);"isqrt" calculates only the integer part, directly discarding the decimals!
 def f(a,b,c):
     return b**2-a*c*4
 a , b , c = map(int, input().split());v = f(a,b,c);ans = "NoSolution"
 if v == 0:
     ans = f"DR={b/(-2*a)}"
 elif v > 0:
-    ans = f"{(-b-isqrt(v))/(2*a)} {(-b+isqrt(v))/(2*a)}"
+    ans = f"{(-b-sqrt(v))/(2*a)} {(-b+sqrt(v))/(2*a)}"
 print(ans)
 #second compose method
 a , b , c = input().split();a = int(a);b = int(b);c = int(c);v = b**2 - a * c *4;ans = "NoSolution"
