3346ea8936be6bebb6e2f29fba6e61bd04863187
Hysilensfan <uraume2027@gmail.com>
2026-01-28 15:01:16 +0800

    Update py 020.py

--- py 020.py
+++ py 020.py
@@ -7,3 +7,7 @@ for _ in range(int(input())):
         else:
             v -= int(s[i])
     print(v)
+for _ in range(int(input())):
+    s = list(input());v = int(s[0])
+    for i in range(1, len(s)):w = lambda :i % 2 != 0;v += int(s[i]) if w() else - int(s[i])
+    print(v)
