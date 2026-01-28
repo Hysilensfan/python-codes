486f3f78d813f18e370ca30a009497320ec8a1ef
Hysilensfan <uraume2027@gmail.com>
2026-01-28 11:33:19 +0800

    Update py 002.py

--- py 002.py
+++ py 002.py
@@ -1,4 +1,4 @@
 #first compose method
 r = float(input());print(f"{r**2*3.14} {r*2*3.14}"if r>0 else "Wrong")
 #second compose method
-from math import pi;s = lambda r:f"{r**2*(int(pi*100)/100)} {r*2*(int(pi*100)/100)}"if r>0 else "Wrong";print(s(float(input()))) #the r=s(float(input()) in function s(r) short for s(float(input()));if u input"5",the output will has deviation
+from math import pi;s = lambda r:f"{r**2*(int(pi*100)/100)} {r*2*(int(pi*100)/100)}"if r>0 else "Wrong";print(s(float(input()))) #s(r) is called with float(input())
