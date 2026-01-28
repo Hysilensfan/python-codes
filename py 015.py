0fad55e0bacb5e20de48d9726ab73b3e42a48aee
Hysilensfan <uraume2027@gmail.com>
2026-01-28 13:57:24 +0800

    Update py 015.py

--- py 015.py
+++ py 015.py
@@ -9,3 +9,7 @@ for i in range(int(input())):
         j += 2
         plus = False
     print(pi)
+for _ in range(int(input())):
+    s = int(input());pi = 3;j = 2;plus = lambda: True
+    for _ in range(s - 1):k = 4 / (j * (j + 1) * (j + 2));pi += k if plus() else -k;j += 2; plus = lambda: not plus()
+    print(pi)
