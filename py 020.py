ba8c8e0ae0f725085de599288c7a146f5b94de98
Hysilensfan <uraume2027@gmail.com>
2026-01-28 15:16:17 +0800

    Update py 020.py

--- py 020.py
+++ py 020.py
@@ -13,3 +13,5 @@ for _ in range(int(input())):
     s = list(input());v = int(s[0])
     for i in range(1, len(s)):w = lambda :i % 2 != 0;v += int(s[i]) if w() else - int(s[i])
     print(v)
+#third compose method
+for _ in range(int(input())):s = input();v = int(s[0]);v += sum(map(lambda i: int(s[i]) if i % 2 != 0 else -int(s[i]), range(1, len(s))));print(v)
