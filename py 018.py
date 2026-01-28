e97b19701668e161e97a370242411c56912ca56b
Hysilensfan <uraume2027@gmail.com>
2026-01-28 15:11:25 +0800

    Update py 018.py

--- py 018.py
+++ py 018.py
@@ -1,5 +1,7 @@
+#first compose method
 for _ in range(int(input())):
     s = list(input());t= 0
     for i in range(len(s)):t += int(s[i]) # to logging every numbers of digits's sum
     print(t)
+#second compose method
 for _ in range(int(input())):s = input();print(sum(map(int,s)))
