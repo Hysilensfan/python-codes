af22e59d5019c7629f0db589bda4c67e9d06685e
Hysilensfan <uraume2027@gmail.com>
2026-01-28 15:37:37 +0800

    Update py 021.py

--- py 021.py
+++ py 021.py
@@ -1,6 +1,5 @@
 for _ in range(int(input())):
     b1 = input();b2 = input();d = 0
     for i in range(len(b1)):
-        if b1[i] != b2[i]:
-            d += 1
+        d += 1 if b1[i] != b2[i] else 0
     print(d)
