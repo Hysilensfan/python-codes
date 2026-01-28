3b50e37e58f6648eb84186945f681a22248025f2
Hysilensfan <uraume2027@gmail.com>
2026-01-28 10:43:43 +0800

    Update py 023.py

--- py 023.py
+++ py 023.py
@@ -1,3 +1,4 @@
+#first compose method
 for i in range(int(input())):
     s = input()
     if s==s[::-1]:
@@ -5,3 +6,4 @@ for i in range(int(input())):
     else:
         s=s.replace(" ","") # equal " s="".join(s.split(" ")) "
         print("Y"if s==s[::-1]else"N")
+#second compose method
