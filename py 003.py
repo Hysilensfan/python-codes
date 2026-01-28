dc58714a7218c89c42ae015d0355749ea93291f6
Hysilensfan <uraume2027@gmail.com>
2026-01-28 11:34:04 +0800

    Update py 003.py

--- py 003.py
+++ py 003.py
@@ -8,4 +8,4 @@ print(ans)
 #second compose method
 y = int(input());print((y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong")
 #third compose method
-y = int(input());calc = lambda y: (y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong";print(calc(y))
+y = int(input());r = lambda y: (y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong";print(r(y))
