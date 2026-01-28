cf0d231a007e502eb6113bf5f52af72cf23339fb
Hysilensfan <uraume2027@gmail.com>
2026-01-28 15:10:57 +0800

    Update py 018.py

--- py 018.py
+++ py 018.py
@@ -2,3 +2,4 @@ for _ in range(int(input())):
     s = list(input());t= 0
     for i in range(len(s)):t += int(s[i]) # to logging every numbers of digits's sum
     print(t)
+for _ in range(int(input())):s = input();print(sum(map(int,s)))
