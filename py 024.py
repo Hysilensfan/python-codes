1489e96f42cc915abb89191314ab5507ece273e4
Hysilensfan <uraume2027@gmail.com>
2026-01-29 13:42:59 +0800

    Update py 024.py

--- py 024.py
+++ py 024.py
@@ -1,11 +1,9 @@
 def f(w):
-    t=0
-    a=1
-    for i in range(1, w+1):
-        a=a*i;t+=a;t=int(str(t)[-6:])
+    t = 0;a = 1
+    for i in range(1, w + 1):
+        a *= i;t = (t + a) % 1000000
     return t
 for i in range(int(input())):
-    w=int(input());r = f(w);v = len(r)
-    if v < 7:
-        v = r.zfill(6)
-    print(f(w))
+    w = int(input());r = str(f(w));
+    if len((r)) < 6:r = r.zfill(6)
+    print(r)
