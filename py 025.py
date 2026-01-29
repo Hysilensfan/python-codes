b5a7f32e975f3ea17a84046f60e4ba248ff9360f
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:18:25 +0800

    Update py 025.py

--- py 025.py
+++ py 025.py
@@ -1,7 +1,7 @@
 def f(w):
     a=1
     for i in range(2, w+1):
-        a=int(str(a*i).rstrip('0'))%100000 # Reduce execution times
+        a=int(str(a*i).rstrip('0'))%100000 # When calculating factorials, first remove the zeros, then keep only the last 5 digits.Reduce execution times
     return a
 for i in range(int(input())):
     w=int(input());v=str(f(w))
