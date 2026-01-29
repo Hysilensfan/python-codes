1d81049e268466f818bedeee289472aa5e3d40a3
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:09:41 +0800

    Update py 001.py

--- py 001.py
+++ py 001.py
@@ -1,4 +1,5 @@
 #first method
-n = int(input());print(f"{n//100} {(n%100)//10} {n%10}" if 1<=n<=999 else "Wrong") #semicolon separates statements; hint:123//100=1;123%100=23,23//10=2;123%10=3
+n = int(input())
+print(f"{n//100} {(n%100)//10} {n%10}" if 1<=n<=999 else "Wrong") #semicolon separates statements; hint:123//100=1;123%100=23,23//10=2;123%10=3
 #second method
 from sys import stdin as f;s=int(f.readline());print("{} {} {}".format(s//100,(s%100)//10,s%10)if 1<=s<=999 else "wrong")
