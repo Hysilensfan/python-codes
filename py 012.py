49732fcc29fa5496278ed0fe36a552785cf11f0a
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:00:13 +0800

    Update py 012.py

--- py 012.py
+++ py 012.py
@@ -1,4 +1,4 @@
-#first compose method
+#first method
 a , b = [int(c) for c in input().split()];print((b - a + 200)%200) # this is an explanation of list comprehension's => read our inputs(a,b) which stored to c, then, convert c into integer(using the int function) so we can using list comprehension:"a , b = [int(c) for c in input().split()])"
-#second compose method
+#second method
 a , b = input().split();print((int(b) - int(a) + 200)%200) # same same result as above,but this code is more readable
