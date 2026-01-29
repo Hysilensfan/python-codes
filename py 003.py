19b0edd2b4397f38058cf67ddba1436098c23635
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:10:18 +0800

    Update py 003.py

--- py 003.py
+++ py 003.py
@@ -1,11 +1,11 @@
-#first compose method
+#first method
 y=int(input());ans="Wrong"
 if y>2:
     ans=(y-2)*4+21
 elif 0<y<=2:
     ans=y*10.5
 print(ans)
-#second compose method
+#second method
 y = int(input());print((y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong")
 #third compose method
 y = int(input());r = lambda y: (y - 2) * 4 + 21 if y > 2 else y * 10.5 if 0 < y <= 2 else "Wrong";print(r(y))
