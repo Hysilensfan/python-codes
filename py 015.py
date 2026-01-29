e9ae09d46acf261d3563dbfbb4234bcdb61e3181
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:11:05 +0800

    Update py 015.py

--- py 015.py
+++ py 015.py
@@ -1,4 +1,4 @@
-#first compose method
+#first method
 for i in range(int(input())):
     s = int(input());j = 2;pi = 3;plus = True
     for i in range(s - 1):
@@ -10,7 +10,7 @@ for i in range(int(input())):
         j += 2
         plus = False
     print(pi)
-#second compose method
+#second method
 for _ in range(int(input())):
     s = int(input());pi = 3;j = 2;plus = lambda: True
     for _ in range(s - 1):k = 4 / (j * (j + 1) * (j + 2));pi += k if plus() else -k;j += 2; plus = lambda: not plus()
