46a854a4964d16a43eff4c92a2ce6ef1e58b8d46
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:30:42 +0800

    Update py 025.py

--- py 025.py
+++ py 025.py
@@ -1,6 +1,6 @@
 def f(w):
     a=1
-    for i in range(2, w+1):
+    for i in range(2, w+1): # hypothesis input(w) is 1 this row won't operation so output 1(default value of a)
         a=int(str(a*i).rstrip('0'))%100000 # When calculating factorials, first remove the zeros, then keep only the last 5 digits.Reduce execution times
     return a
 for i in range(int(input())):
