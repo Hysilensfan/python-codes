419b159e3e8aaca2339ab4f93d77307f65ea0831
Hysilensfan <uraume2027@gmail.com>
2026-01-29 10:52:02 +0800

    Update py 014.py

--- py 014.py
+++ py 014.py
@@ -1,22 +1,22 @@
 n = int(input())
-for i in range(1,n+1): #controling number of column
-    for u in range(1,i+1): #controling internal loop and print 1 to i
+for i in range(1,n+1): #control number of rows
+    for u in range(1,i+1): #controlling internal loop and print 1 to i
         print(u,end=' ') #print digits and don't turn to newline
     print() #newline
-print() #newline
+print()
 for i in range(1,n+1):
-    for u in range(1,n-i+2): # #controling internal roop and print 1 to n-i+1
+    for u in range(1,n-i+2): #each row prints one less number than the previous
         print(u,end=' ')
     print()
 print()
 for i in range(1,n+1):
-    for u in range(n,i-1,-1): # controling internal roop and print 1 to n-i-2,interval -1
+    for u in range(n,i-1,-1): # print numbers from n down to i
         print(u,end=' ')
     print()
 print()
 for i in range(1,n+1):
-    for j in range(n-i): # controling internal roop and print 0(computer default 0 is first) to n-i-1
-        print(' ',end=' ')
-    for j in range(1,i+1): # controling internal roop and print 1 to i
-        print(j,end=' ')
+    for j in range(n-i): # repeat n-i times
+        print(' ',end=' ') #printing spaces for right alignment
+    for j in range(1,i+1): # controlling internal loop and print 1 to i
+        print(j,end=' ') # print numbers from 1 to i
     print()
