3541817f1af812e4fe91733bb2cb0e4bce996d99
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:22:08 +0800

    Update py 010.py

--- py 010.py
+++ py 010.py
@@ -7,11 +7,5 @@ elif v > 0:
 print(ans)
 #second method
 from math import sqrt        #"sqrt" calculates the real square root (with decimals);"isqrt" calculates only the integer part, directly discarding the decimals!
-def f(a,b,c):
-    return b**2-a*c*4
-a , b , c = map(int, input().split());v = f(a,b,c);ans = "NoSolution"
-if v == 0:
-    ans = f"DR={b/(-2*a)}"
-elif v > 0:
-    ans = f"{(-b-sqrt(v))/(2*a)} {(-b+sqrt(v))/(2*a)}"
-print(ans)
+def f(a,b,c):return b**2-a*c*4
+a , b , c = map(int, input().split());v = f(a,b,c);print(f"DR={b/(-2*a)}" if v == 0 else f"{(-b-sqrt(v))/(2*a)} {(-b+sqrt(v))/(2*a)}" if v > 0 else"NoSolution"))
