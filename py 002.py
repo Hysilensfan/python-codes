31119f5154c2a842223a4b620041dd22afd78efe
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:10:08 +0800

    Update py 002.py

--- py 002.py
+++ py 002.py
@@ -1,4 +1,4 @@
-#first compose method
+#first method
 r = float(input());print(f"{r**2*3.14} {r*2*3.14}"if r>0 else "Wrong")
-#second compose method
+#second method
 from math import pi;s = lambda r:f"{r**2*(int(pi*100)/100)} {r*2*(int(pi*100)/100)}"if r>0 else "Wrong";print(s(float(input()))) #s(r) is called with float(input())
