7fafb532775876724b244b290be20a64a44b663c
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:13:49 +0800

    Update py 010.py

--- py 010.py
+++ py 010.py
@@ -1,4 +1,11 @@
 #first method
+a , b , c = input().split();a = int(a);b = int(b);c = int(c);v = b**2 - a * c *4;ans = "NoSolution"
+if v == 0:
+    ans = f"DR={b/(-2*a)}"
+elif v > 0:
+    ans = f"{(-b-v**0.5)/(2*a)} {(-b+v**0.5)/(2*a)}"
+print(ans)
+#second method
 from math import sqrt        #"sqrt" calculates the real square root (with decimals);"isqrt" calculates only the integer part, directly discarding the decimals!
 def f(a,b,c):
     return b**2-a*c*4
@@ -7,11 +14,4 @@ if v == 0:
     ans = f"DR={b/(-2*a)}"
 elif v > 0:
     ans = f"{(-b-sqrt(v))/(2*a)} {(-b+sqrt(v))/(2*a)}"
-print(ans)
-#second method
-a , b , c = input().split();a = int(a);b = int(b);c = int(c);v = b**2 - a * c *4;ans = "NoSolution"
-if v == 0:
-    ans = f"DR={b/(-2*a)}"
-elif v > 0:
-    ans = f"{(-b-v**0.5)/(2*a)} {(-b+v**0.5)/(2*a)}"
 print(ans)
