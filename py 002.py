5513d5356866b665608c3dad67db7f060a1e85db
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:09:26 +0800

    Update py 002.py

--- py 002.py
+++ py 002.py
@@ -1,4 +1,5 @@
 #first method
-r = float(input());print(f"{r**2*3.14} {r*2*3.14}"if r>0 else "Wrong")
+r = float(input())
+print(f"{r**2*3.14} {r*2*3.14}"if r>0 else "Wrong")
 #second method
 from math import pi;s = lambda r:f"{r**2*(int(pi*100)/100)} {r*2*(int(pi*100)/100)}"if r>0 else "Wrong";print(s(float(input()))) #s(r) is called with float(input())
