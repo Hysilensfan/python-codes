f6041b4ba3b226c735ac9d0f499e0df11a8e376e
Hysilensfan <uraume2027@gmail.com>
2026-01-29 11:11:59 +0800

    Update py 020.py

--- py 020.py
+++ py 020.py
@@ -1,4 +1,4 @@
-#first compose method
+#first method
 for _ in range(int(input())):
     s = list(input())
     v = int(s[0])
@@ -8,10 +8,10 @@ for _ in range(int(input())):
         else:
             v -= int(s[i])
     print(v)
-#second compose method
+#second method
 for _ in range(int(input())):
     s = list(input());v = int(s[0])
     for i in range(1, len(s)):w = lambda :i % 2 != 0;v += int(s[i]) if w() else - int(s[i]) # True if i is odd
     print(v)
-#third compose method
+#third method
 for _ in range(int(input())):s = input();v = int(s[0]);v += sum(map(lambda i: int(s[i]) if i % 2 != 0 else -int(s[i]), range(1, len(s))));print(v) 
