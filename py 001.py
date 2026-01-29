fe36e4ca13f99a97b4a13fc8e5cd016408b43d7f
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:06:12 +0800

    Update py 001.py

--- py 001.py
+++ py 001.py
@@ -1,1 +1,1 @@
-n = int(input());print(f"{n//100} {(n%100)//10} {n%10}" if 1<=n<=999 else "wrong") #semicolon separates statements; hint:123//100=1;123%100=23,23//10=2;123%10=3
+n = int(input());print(f"{n//100} {(n%100)//10} {n%10}" if 1<=n<=999 else "Wrong") #semicolon separates statements; hint:123//100=1;123%100=23,23//10=2;123%10=3
