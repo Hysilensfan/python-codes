c86db41a811705020f2bcb2cba89a5e7f4e3c4ee
Hysilensfan <uraume2027@gmail.com>
2026-01-29 12:10:01 +0800

    Update py 024.py

--- py 024.py
+++ py 024.py
@@ -1,1 +1,11 @@
-print(r)
+def f(w):
+    t=0
+    a=1
+    for i in range(1, w+1):
+        a=a*i;t+=a;t=int(str(t)[-6:])
+    return t
+for i in range(int(input())):
+    w=int(input());r = f(w);v = len(r)
+    if v < 7:
+        v = r.zfill(6)
+    print(f(w))
