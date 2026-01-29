44cc94ffc6824365a738aea44af0704beed81001
Hysilensfan <uraume2027@gmail.com>
2026-01-30 01:36:05 +0800

    Update py 029.py

--- py 029.py
+++ py 029.py
@@ -1,7 +1,7 @@
 from sys import stdin as c
-for line in c:
+for i in c:
     try:
-        a, b = map(int, line.split())
+        a, b = map(int, i.split())
         print(4 * a + 6 * b)
     except:
         pass
