0cd193ac8a3525ca74ed780b0e5c6ae73902775a
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:42:52 +0800

    Update py 015.py

--- py 015.py
+++ py 015.py
@@ -1,17 +1,14 @@
 #first method
 for i in range(int(input())):
-    s = int(input());j = 2;pi = 3;plus = True
-    for i in range(s - 1):
-        k = (4 / (j * (j + 1) * (j + 2))) # r = 4/(2n*(2n+1)*(2n+2))
-        if plus:
-            pi += k
-        else:
-            pi -= k
-        j += 2
-        plus = False
-    print(pi)
-#second method
-for _ in range(int(input())):
-    s = int(input());pi = 3;j = 2;plus = lambda: True
-    for _ in range(s - 1):k = 4 / (j * (j + 1) * (j + 2));pi += k if plus() else -k;j += 2; plus = lambda: not plus() # reverse the boolean value
-    print(pi)
+    e: int = int(input())
+    tot, ope, k = 3, 1, 2
+    if e > 1:
+        for p in range(e - 1):
+            if ope == 1:
+                tot += 4 / (k * (k + 1) * (k + 2))
+                ope = 0
+            elif ope == 0:
+                tot -= 4 / (k * (k + 1) * (k + 2))
+                ope = 1
+            k += 2
+    print(tot)
