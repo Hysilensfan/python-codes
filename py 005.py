09405254db123c9a3840a7a247699637dead1820
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:23:47 +0800

    Update py 005.py

--- py 005.py
+++ py 005.py
@@ -4,8 +4,8 @@ H_conver: int = 60 ** 2
 M_conver: int = 60
 print(f"{s // D_conver}D{s % D_conver // H_conver}H{s % (D_conver) % (H_conver)// M_conver}M{s % (D_conver) % (H_conver) % M_conver}S")
 """
-1day = 24hour
-1hour = 60minute
-1minute = 60second
-1day = 24hourx60minutex60second = 86400second
+1day = 24hours
+1hour = 60minutes
+1minute = 60seconds
+1day = 24hour x 60minute x 60second = 86400seconds
 """
