dbf6e9a6efcd7d149e212ad9c2d50825546f0de8
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 01:12:37 +0800

    Update py 032.py

--- py 032.py
+++ py 032.py
@@ -1,6 +1,7 @@
 #first method
 for _ in range(int(input())):
-    a = input();y = 0
+    a = input()
+    y = 0
     if len(a) == 16:
         for i in range(len(a)):
             if i % 2 == 0:
@@ -20,8 +21,5 @@ for _ in range(int(input())):
         print("F")
 #second method
 for _ in range(int(input())):
-    a = input()
-if (sum(map(int,str(int(a[0])*2))) + sum(map(int,str(int(a[1])*1))) + sum(map(int,str(int(a[2])*2))) + sum(map(int,str(int(a[3])*1))) + sum(map(int,str(int(a[4])*2))) + sum(map(int,str(int(a[5])*1))) + sum(map(int,str(int(a[6])*2))) + sum(map(int,str(int(a[7])*1))) + sum(map(int,str(int(a[8])*2))) + sum(map(int,str(int(a[9])*1))) + sum(map(int,str(int(a[10])*2))) + sum(map(int,str(int(a[11])*1))) + sum(map(int,str(int(a[12])*2))) + sum(map(int,str(int(a[13])*1))) + sum(map(int,str(int(a[14])*2))) + sum(map(int,str(int(a[15])*1))))%10==0 and len(a)==16:
-    print("T")
-else:
-    print("F")
+    a: str = input()
+    print('T' if (sum(map(int,str(int(a[0])*2))) + sum(map(int,str(int(a[1])*1))) + sum(map(int,str(int(a[2])*2))) + sum(map(int,str(int(a[3])*1))) + sum(map(int,str(int(a[4])*2))) + sum(map(int,str(int(a[5])*1))) + sum(map(int,str(int(a[6])*2))) + sum(map(int,str(int(a[7])*1))) + sum(map(int,str(int(a[8])*2))) + sum(map(int,str(int(a[9])*1))) + sum(map(int,str(int(a[10])*2))) + sum(map(int,str(int(a[11])*1))) + sum(map(int,str(int(a[12])*2))) + sum(map(int,str(int(a[13])*1))) + sum(map(int,str(int(a[14])*2))) + sum(map(int,str(int(a[15])*1))))%10==0 and len(a)==16 else 'F')
