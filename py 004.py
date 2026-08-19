8d68b580a3c5f86cf0f4939288fd2bef23316be3
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:14:37 +0800

    Refactor year check with type annotations

--- py 004.py
+++ py 004.py
@@ -1,5 +1,5 @@
-y=int(input())
-ans="a normal year"
-if (y%4==0 and y%100!=0) or (y%400==0):
-    ans="a leap year"
+y: int = int(input())
+ans: str = "a normal year"
+if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+    ans: str = "a leap year"
 print(ans)
