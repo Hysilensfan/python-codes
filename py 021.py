cf6b2b4a0696700c15e8beac944d217a7c487e64
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:57:26 +0800

    Refactor input and count differences in binary strings
    Refactor input handling and improve readability of methods.

--- py 021.py
+++ py 021.py
@@ -1,5 +1,10 @@
+# First method
 for _ in range(int(input())):
-    b1 = input();b2 = input();d = 0
+    b1, b2 = [input() for _ in range(2]
+    d: int = 0
     for i in range(len(b1)):
         d += 1 if b1[i] != b2[i] else 0
     print(d)
+
+# Second method
+[print(bin(int(input(),2) ^ int(input(),2)).count("1"))for k in range(int(input()))]
