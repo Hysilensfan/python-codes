b2d9ec48ad487bb7ac0b0cf18373001c147d0466
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:46:07 +0800

    Update py 017.py

--- py 017.py
+++ py 017.py
@@ -1,26 +1,30 @@
 #first method
 for _ in range(int(input())):
-    s = int(input());t = True
+    s: int = int(input())
+    t: bool = True
     if s < 2:
-        t = False
+        t: bool = False
     else:
-        for i in range(2, int(s**0.5)+1): #using the square root for save calculation times,because this problem descripition that can only  be calculated in 1 second
+        for i in range(2, int(s**0.5) + 1): #using the square root for save calculation times,because this problem descripition that can only  be calculated in 1 second
             if s % i == 0:
-                t = False
+                t: bool = False
                 break
-    print("Y"if t else"N")
+    print("Y" if t else"N")
 #second method
 def f(s):
     if s < 2:
         return False
     else:
-        for i in range(2, int(s**0.5)+1):
+        for i in range(2, int(s**0.5) + 1):
             if s % i == 0:
                 return False
                 break
         return True
-for i in range(int(input())):
-    s = int(input());print("Y"if f(s) else"N")
+
+
+for _ in range(int(input())):
+    print("Y" if f(int(input())) else"N")
 #third method
-from math import sqrt;f = lambda s:s>1 and all(s % i != 0 for i in range(2, int(sqrt(s))+1))  #lamda belike define function.any Determine if an object is iterable and check if one of its elements is True.
-for i in range(int(input())):s = int(input());print("Y"if f(s) else"N")
+from math import sqrt
+f = lambda s:s > 1 and all(s % i != 0 for i in range(2, int(sqrt(s)) + 1))  #lamda belike define function.any Determine if an object is iterable and check if one of its elements is True.
+[print("Y" if f(int(input())) else"N") for _ in range(int(input()))]
