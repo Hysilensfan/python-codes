262a7446e118229ddf884aa9e6c5013f6f42ad41
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:54:27 +0800

    Update py 019.py

--- py 019.py
+++ py 019.py
@@ -1,10 +1,18 @@
-#first method
+# First method
 for _ in range(9):
-    n=  input();tT = tL = 0 #default counting total value is 0
+    n: str =  input()
+    tT = tL = 0 # Default counting total value is 0
     if n == "Tiger":
         tT += 1
     else:
         tL += 1
 print("Tiger" if tT > tL else "Lion")
-#second method
-print("Tiger" if sum(input()=="Tiger" for _ in range(9)) > 4 else "Lion") #logic clearly version,just judges the "Tiger"'s appeared dights greater than the "Lion"'s appeared dights
+# Second method
+print("Tiger" if sum(input()=="Tiger" for _ in range(9)) > 4 else "Lion")
+"""
+Logic clearly version,just judges the "Tiger"'s appeared dights greater than the "Lion"'s appeared dights.
+"""
+
+# Third method
+log: list = [input() for _ in range(9)]
+print("Tiger" log.counter("Tiger") > 4 else  "Lion")
