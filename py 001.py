eda9746882bb5fb12a33a193ab1f23a5f2b8ceaf
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:13:20 +0800

    Update py 001.py

--- py 001.py
+++ py 001.py
@@ -1,9 +1,15 @@
 # first method
-n = int(input())
-print(f"{n // 100} {(n % 100) // 10} {n % 10}" if 1 <= n <= 999 else "Wrong")  # hint:123//100=1;123%100=23,23//10=2;123%10=3
+n: int = int(input())
+print(f"{n // 100} {(n % 100) // 10} {n % 10}" if 1 <= n <= 999 else "Wrong")
+"""
+hint:
+123 // 100 = 1
+123 % 100=23, 23 // 10 = 2
+123 % 10 = 3
+"""
 # second method
-s = int(input())
+s: int = int(input())
 print("{} {} {}".format(s // 100, (s % 100) // 10, s % 10) if 1 <= s <= 999 else "wrong")
 # third method
-k = int(input())
+k: int = int(input())
 print("%d %d %d" % (s // 100, (s % 100) // 10, s % 10) if 1 <= s <= 999 else "wrong")
