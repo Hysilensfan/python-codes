589ce4d7eeb5eca53ac5a79c2216b37fe1328239
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 01:09:56 +0800

    Fix formatting and improve readability in py 030.py

--- py 030.py
+++ py 030.py
@@ -1,7 +1,8 @@
 for i in range(int(input())):
-    n= input()
+    n = input()
     while True:
-        if n!=n[::-1]:
-            n=str(int(n)+int(n[::-1]))
+        if n != n[::-1]:
+            n=str(int(n) + int(n[::-1]))
         else:
-            print(n);break
+            print(n)
+            break
