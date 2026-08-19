edc6b1a7ab3c67cb502ff950f2883720e2fd9818
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 00:57:45 +0800

    Update py 021.py

--- py 021.py
+++ py 021.py
@@ -3,7 +3,7 @@ for _ in range(int(input())):
     b1, b2 = [input() for _ in range(2]
     d: int = 0
     for i in range(len(b1)):
-        d += 1 if b1[i] != b2[i] else 0
+        d = d + 1 if b1[i] != b2[i] else 0
     print(d)
 
 # Second method
