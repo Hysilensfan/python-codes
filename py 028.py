6ada56510cb522dd2f76e6d7602b746677d5b4f8
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 01:09:12 +0800

    Update py 028.py

--- py 028.py
+++ py 028.py
@@ -1,15 +1,30 @@
-def happy(s):
-    n = set()
+# First method
+def happy(s: int):
+    n: set = set()
     while s != 1:
         if s in n:
             break
-        n.add(s);t=0
-        for e in range(len(str(s))):
-            t += int(str(s)[e])**2
+        n.add(s)
+        t = 0
+        for e in range(len(str(s))):  # Digit sum.
+            t += int(str(s)[e]) ** 2
         s = t
     else:
         return True
     return False
+
+
 for i in range(int(input())):
-    s = int(input())
-    print("T" if happy(s)else"F")
+    print("T" if happy(int(input())) else"F")
+
+# Second method
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        dire: set = set()
+        while n != 1 and n not in dire:
+            dire.add(n)
+            n: int = sum(int(c) ** 2 for c in str(n))
+        return n == 1
+
+
+[print("T" if happy(int(input())) else"F") for _ in range(int(input()))]
