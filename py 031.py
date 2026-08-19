db885fe3a5a2ecbf910739416ed89b921b00c1e9
霧流羽kiriha <uraume2027@gmail.com>
2026-08-20 01:10:44 +0800

    Update py 031.py

--- py 031.py
+++ py 031.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
     a, b = input() for _ in range(2)]
-    print("N" if set(a).isdisjoint(b) else"".join(sorted(set(b).intersection(a))))
+    print('N' if set(a).isdisjoint(b) else"".join(sorted(set(b).intersection(a))))
