a84866b102acab6d1297ab991300d234f51e09b0
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 00:42:33 +0800

    Update py 030.py

--- py 030.py
+++ py 030.py
@@ -1,8 +1,15 @@
+# First method
 for i in range(int(input())):
-    n = input()
+    n: str = input()
     while True:
         if n != n[::-1]:
-            n=str(int(n) + int(n[::-1]))
+            n: str = str(int(n) + int(n[::-1]))
         else:
             print(n)
             break
+
+# Second method
+echo = lambda s: s if str(s) == str(s)[::-1] else echo(s + int(str(s)[::-1]))
+
+[print(echo(int(input()))) for _ in range(int(input()))]
+
