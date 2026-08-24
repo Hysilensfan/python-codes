487896c3a924dbe7a699e219b3d565aeb9a90e33
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 00:23:20 +0800

    Update py 024.py

--- py 024.py
+++ py 024.py
@@ -5,7 +5,7 @@ def factorial(x):
         h: int = 1
         for i in range(1, c + 1):
             h *= i
-        tot = (tot + h) % 1000000
+        tot: int = (tot + h) % 1000000
     return str(tot).zfill(6)
 
 
@@ -13,11 +13,11 @@ for k in range(int(input())):
     print(factorial(int(input())))
 
 # Second method
-recurtion = lambda w: w * recurtion(w - 1) if w != 0 else 1
+recurtion = lambda w : w * recurtion(w - 1) if w != 0 else 1
 
 
-def factorial(r):
-    tot = 1
+def factorial(r: int) -> str:
+    tot: int = 1
     if r == 1:
         return "000001"
     elif r == 22 or r >= 24:
@@ -25,15 +25,18 @@ def factorial(r):
     elif 24 > r > 1:
         if r > 1:
             while r > 1:
-                tot = (tot + recurtion(r)) % 1000000
+                tot: int = (tot + recurtion(r)) % 1000000
                 r -= 1
         return str(tot).zfill(6)
+    return ""
 
 
 [print(factorial(int(input()))) for _ in range(int(input()))]
 
 # Third method
-f = lambda w, a = 1, t = 0: ([t := (t + (a := a * i)) % 1000000 for i in range(1, w + 1)] and t) # commas here only split parameters, not like ; that separates multiple statements.by the way, here "and" does not for logical operationit means the list had a value,so t is returned
+f = lambda w, a = 1, t = 0: ([t := (t + (a := a * i)) % 1000000 for i in range(1, w + 1)] and t) # Here "and" does not for logical operationit means the list had a value,so t is returned
+
+
 for _ in range(int(input())):
     r: str = str(f(int(input())))
     print(r := r.zfill(6) if len(r) < 6 else r)
