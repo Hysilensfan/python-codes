0f836952d1e241f51371e6c6fe6a9e5d95e3c610
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 00:26:29 +0800

    Update py 022.py

--- py 022.py
+++ py 022.py
@@ -1,6 +1,4 @@
 for i in range(int(input())):
     a, b = input().split()
-    for e in a:
-        c = (ord(e) - ord('A') + int(b)) % 26 + ord('A')
-        print(chr(c), end="")
+    [print(chr((ord(e) - ord('A') + int(b)) % 26 + ord('A')), end="") for e in a]
     print()
