c91ea104e9542673e8d9598dd32619010395c74c
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 02:27:17 +0800

    Update py 032.py

--- py 032.py
+++ py 032.py
@@ -1,25 +1,25 @@
-#first method
+# First method
 for _ in range(int(input())):
-    a = input()
-    y = 0
+    a: str = input()
+    tot: int = 0
     if len(a) == 16:
         for i in range(len(a)):
-            if i % 2 == 0:
+            if i % 2 == 0:  # Even digit position
                 d= int(a[i]) * 2
                 if d > 9:
                     d -= 9
-                else:
-                    d = d
             else:
-                d= int(a[i])
-            y += d
-        if y % 10 == 0:
-            print("T")
+                d: int = int(a[i])
+            tot += d
+        if tot % 10 == 0:
+            print('T')
         else:
-            print("F")
+            print('F')
     else:
-        print("F")
-#second method
-for _ in range(int(input())):
-    a: str = input()
-    print('T' if (sum(map(int,str(int(a[0])*2))) + sum(map(int,str(int(a[1])*1))) + sum(map(int,str(int(a[2])*2))) + sum(map(int,str(int(a[3])*1))) + sum(map(int,str(int(a[4])*2))) + sum(map(int,str(int(a[5])*1))) + sum(map(int,str(int(a[6])*2))) + sum(map(int,str(int(a[7])*1))) + sum(map(int,str(int(a[8])*2))) + sum(map(int,str(int(a[9])*1))) + sum(map(int,str(int(a[10])*2))) + sum(map(int,str(int(a[11])*1))) + sum(map(int,str(int(a[12])*2))) + sum(map(int,str(int(a[13])*1))) + sum(map(int,str(int(a[14])*2))) + sum(map(int,str(int(a[15])*1))))%10==0 and len(a)==16 else 'F')
+        print('F')
+# Second method
+weight: tuple = (2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1)
+credit_card = lambda s: 'T' if divmod(sum([sum(map(int, str(x))) * y for x, y in zip(s, weight)]), 10)[1] and len(
+    s) == 16 else 'F'  # divmod() function will return tuple that include quotient and remainder from two parameters.
+
+[print(credit_card(input())) for _ in range(int(input()))]
