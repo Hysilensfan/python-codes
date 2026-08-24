08f5f92de76e6cd9c798038aa6b546e5fdcc9100
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 00:20:26 +0800

    Update py 024.py

--- py 024.py
+++ py 024.py
@@ -33,7 +33,7 @@ def factorial(r):
 [print(factorial(int(input()))) for _ in range(int(input()))]
 
 # Third method
-f = lambda w, a = 1, t = 0: ([t := (t + (a := a * i)) % 1000000 for i in range(1, w+1)] and t) # commas here only split parameters, not like ; that separates multiple statements.by the way, here "and" does not for logical operationit means the list had a value,so t is returned
+f = lambda w, a = 1, t = 0: ([t := (t + (a := a * i)) % 1000000 for i in range(1, w + 1)] and t) # commas here only split parameters, not like ; that separates multiple statements.by the way, here "and" does not for logical operationit means the list had a value,so t is returned
 for _ in range(int(input())):
     r: str = str(f(int(input())))
     print(r := r.zfill(6) if len(r) < 6 else r)
