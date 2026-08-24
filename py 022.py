416a7b67fc8ed3180be90d74641ea80f8d9c52cb
霧流羽kiriha <uraume2027@gmail.com>
2026-08-25 00:26:39 +0800

    Update py 022.py

--- py 022.py
+++ py 022.py
@@ -1,4 +1,4 @@
-for i in range(int(input())):
+for _ in range(int(input())):
     a, b = input().split()
     [print(chr((ord(e) - ord('A') + int(b)) % 26 + ord('A')), end="") for e in a]
     print()
