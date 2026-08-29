e61f83b42af7a6302b9d058d5fb5d1d30eff1a0a
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:35:32 +0800

    Update py 043.py

--- py 043.py
+++ py 043.py
@@ -1,1 +1,7 @@
+# First method
+for _ in range(int(input())):
+    x, y, m = map(int, input().split())
+    print(x ** y % m)
 
+# Second method
+[print(pow(x, y, m)) for _ in range(int(input())) for x, y, m in [map(int, input().split())]]
