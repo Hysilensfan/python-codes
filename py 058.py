428585ac8397b56870525a9321bac5d37c37a995
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:02:34 +0800

    Update py 058.py

--- py 058.py
+++ py 058.py
@@ -1,9 +1,16 @@
-def operation(f):
-    f = f.replace("-", "+-").split("+");k=  0
+# First method
+def operation(f: str) -> int:
+    f: str = f.replace("-", "+-").split("+")
+    k: int =  0
     for n in f:
-        if n!= "":
-            k+=int(n.strip(" "))
+        if n != "":
+            k += int(n.strip())
     return k
+
+
 for i in range(4):
-    a,c = input().split("==")
-    print("TRUE"if operation(a) == operation(c)else"FALSE")
+    a, c = input().split("==")
+    print("TRUE" if operation(a) == operation(c) else "FALSE")
+
+# Second method
+[print("TRUE" if eval(input()) else "FALSE") for _ in range(4)]
