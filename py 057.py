cc6450c6968bd3e589c6a9a3f182aaa390207d82
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:04:09 +0800

    Update py 057.py

--- py 057.py
+++ py 057.py
@@ -1,6 +1,4 @@
-# first method
+# First method
 from ast import literal_eval as a
+
 print((str(list(map(list,zip(*(a(input())[::-1])))))).replace(" ",""))
-# second method
-from ast import literal_eval as l
-print(str([*[list(a) for a in zip(*reversed(l(input())))]]).replace(" ", ""))
