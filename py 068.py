0185f6082aa82c105c61b141b2571ac92ba54250
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:20:01 +0800

    Update py 068.py

--- py 068.py
+++ py 068.py
@@ -1,11 +1,16 @@
-def int_to_roman(s):
-    ex = {
+ex = {
         1000:"M",900:"CM", 500:"D", 400:"CD",
         100:"C", 90:"XC", 50:"L", 40:"XL",
         10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"
     }
-    r = ''
+
+def int_to_roman(s: int) -> str:
+    r: str = ""
     for i in ex.keys():
-        while  s>= i:s-= i;r+=ex[i]
+        while s >= i:
+            s -= i
+            r += ex[i]
     return r
-for _ in range(int(input())):print(int_to_roman(int(input())))
+
+
+[print(int_to_roman(int(input()))) for _ in range(int(input()))]
