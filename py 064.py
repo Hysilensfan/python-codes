95d4c929585f89d752380c13d6804a1b9f90aa79
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:06:11 +0800

    Fix variable unpacking and type hinting in 064.py

--- py 064.py
+++ py 064.py
@@ -1,5 +1,7 @@
 from itertools import permutations as p
+
+
 for i in range(int(input())):
-    i,j,k = input().split(',');j,k=int(j),int(k)
-    sort_that = sorted(int(''.join(p)) for p in list(p(i)))
+    i, j, k = input().split(',');j,k=int(j),int(k)
+    sort_that: list = sorted(int(''.join(p)) for p in list(p(i)))
     print(sort_that[j-1]+sort_that[k-1])
