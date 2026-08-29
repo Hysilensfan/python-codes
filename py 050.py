f4adbb61057fefa26c3cdd75772612ae3e9e5ca3
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:25:41 +0800

    Update py 050.py

--- py 050.py
+++ py 050.py
@@ -1,2 +1,6 @@
-a, b = list(map(int, input().split(" "))), list(map(int, input().split(" ")))
+# First method
+a, b = [list(map(int, input().split())) for _ in range(2]
 print(a[0] * b[0] + a[1] * b[1] + a[2] * b[2])
+
+# Second method
+print(sum(a * b for a, b in zip(*[map(int, input().split()) for _ in range(2)])))
