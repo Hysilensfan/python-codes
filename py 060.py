2d19a1a317f052ea18e0d41a49c9a3a2679af4e8
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:55:47 +0800

    Update py 060.py

--- py 060.py
+++ py 060.py
@@ -1,17 +1,24 @@
-# first method
-def recurtion(w):
-    t = 1
+# First method
+def recurtion(w: int) -> int:
+    t: int = 1
     if w != 0 and w!= 1:
         for i in range(1, w + 1):
             t *= i
         return t
     else:
         return 1
+
+
 for _ in range(int(input())):
-    c = int(input())
+    c: int = int(input())
     print(recurtion(c))
 
-#second method
+# Second method
 recurtion = lambda w:  w * recurtion(w-1) if w != 0 else 1
-for _ in range(int(input())):
-    print(recurtion(int(input())))
+
+[print(recurtion(int(input()))) for _ in range(int(input()))]
+
+# Third method
+from math import factorial as f
+
+[print(f(int(input()))) for _ in range(int(input()))]
