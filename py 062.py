3306e2ba65f6430481fd1799976e17fe7331c9f2
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:50:29 +0800

    Update py 062.py

--- py 062.py
+++ py 062.py
@@ -1,9 +1,20 @@
+# First method
 gcd = lambda a, b: gcd(b, a % b) if a % b != 0 else b
 lcm = lambda f, g: f * g // gcd(f, g)
+
+
 for _ in range(int(input())):
-    d = list(map(int, input().split(',')))
+    d: list = list(map(int, input().split(',')))
     c = l = d[0]
     for e in d[1:]:
-        c = gcd(c, e)
-        l = lcm(l, e)
+        c: int = gcd(c, e)
+        l: int = lcm(l, e)
     print(f"{c} {l}")
+
+# Second method
+from math import gcd as g_
+from math import lcm as l
+
+for _ in range(int(input())):
+    d: list[int] = list(map(int, input().split(',')))
+    print(g_(*d), l(*d))
