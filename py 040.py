fcea32680c0863faca2063c50b061bdac64579c2
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:37:45 +0800

    Refactor loop and variable annotations in 040.py

--- py 040.py
+++ py 040.py
@@ -1,8 +1,10 @@
-for e in range(int(input())):
-    n = input();tot = 0;count_O=0
+for _ in range(int(input())):
+    n: str = input()
+    tot: int = 0
+    count_O: int = 0
     for c in n:
-        if c == "O":
-            tot = tot + 1 + count_O
+        if c == 'O':
+            tot += 1 + count_O
             count_O += 1
         else:
             count_O = 0
