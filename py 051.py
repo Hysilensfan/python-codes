af377cc236a961166ba17abb45df1146cd7ef48c
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:10:53 +0800

    Refactor dict_ initialization and input processing
    Refactor dict_ to use type hinting and improve input handling.

--- py 051.py
+++ py 051.py
@@ -1,7 +1,10 @@
 from collections import Counter as C
-dict_ = []
+
+dict_: list = []
 for i in range(int(input())):
-    dict_.append(input().split(' ')[0])
+    dict_.append(input().split()[0])
+
 k = list(sorted(C(dict_).items()))
+
 for c in range(len(k)):
     print(f"{k[c][0]} {k[c][1]}" )
