dd6f0f4cce918c2fd908935c3ff9a411eb0c939d
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:07:10 +0800

    Add type annotations for variables in py 066.py

--- py 066.py
+++ py 066.py
@@ -1,5 +1,7 @@
 for _ in range(int(input())):
-    N = (bin(int(input()))).replace("0b","");tot = 0
+    N: str = (bin(int(input()))).replace("0b", "")
+    tot: int = 0
     for c in range(len(N)):
-        if N[c] == '1':tot+=1
+        if N[c] == '1':
+            tot += 1
     print(tot)
