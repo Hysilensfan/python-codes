624144af63f0623a9acb67660a4902b61f2afc62
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:06:07 +0800

    Update py 053.py

--- py 053.py
+++ py 053.py
@@ -1,10 +1,10 @@
-for i in range(int(input())):
-    list_digit = [0,0,0,0,0,0,0,0,0,0]
-    h = ""
-    for j in range(1,int(input())+1):
-        h +=str(j)
+for _ in range(int(input())):
+    list_digit: list[int] = [0] * 9
+    h: str = ""
+    for j in range(1, int(input())+1):
+        h += str(j)
     for w in range(len(list_digit)):
         for k in h:
             if k == str(w):
-                list_digit[w]+=1
-    print(*list_digit,sep = " ")
+                list_digit[w] += 1
+    print(*list_digit, sep=' ')
