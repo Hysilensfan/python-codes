a0a2fc264e2cca42903b16d6552ef0e4ad50827e
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 21:53:23 +0800

    Update py 061.py

--- py 061.py
+++ py 061.py
@@ -1,6 +1,8 @@
-def gcd(a, b):
-    r = a % b
+# First method
+def gcd(a: int, b: int) -> int:
+    r: int = a % b
     return gcd(b, r) if r != 0 else b
+
 
 for _ in range(int(input())):
     d = list(map(int, input().split(',')))
@@ -8,3 +10,8 @@ for _ in range(int(input())):
     for e in d[1:]:
         c = gcd(c, e)
     print(c)
+
+# Second method
+from math import gcd
+
+[gcd(*map(int, input().split(','))) for _ in range(int(input()))]
