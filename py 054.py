4c039758c555e2f828fa154197434bf77050e129
霧流羽kiriha <uraume2027@gmail.com>
2026-08-29 22:05:15 +0800

    Update py 054.py

--- py 054.py
+++ py 054.py
@@ -1,10 +1,16 @@
 while True:
-    a,b = map(int,input().split())
-    if a == 0 and b == 0:break
-    tot = plus = 0;length = max(len(str(a)), len(str(b)));a, b = str(a).zfill(length), str(b).zfill(length)
+    a, b = map(int,input().split())
+    if a == 0 and b == 0:
+        break
+    tot = plus = 0
+    length = max(len(str(a)), len(str(b)))
+    a, b = str(a).zfill(length), str(b).zfill(length)
     for i in range(length):
-        if ((int(a[i]) + int(b[i]))+plus)>=10:plus=1;tot += 1
-        else:plus=0
+        if ((int(a[i]) + int(b[i]))+plus)>=10:
+            plus=1
+            tot += 1
+        else:
+            plus=0
     if 2 > tot > 0:
         print(f"{tot} carry operation.")
     elif tot > 1:
